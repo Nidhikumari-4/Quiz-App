60b22633347451c27bf9c4e3ad469e77ced8cb1e
Nidhi Kumari <kumarinidhimain@gmail.com>
2023-04-07 00:06:44 +0530

    +

--- src/components/CreateQuizButton.jsx
+++ src/components/CreateQuizButton.jsx
@@ -5,7 +5,7 @@ const CreateQuizButton = () => {
   return (
     <div>
       <Link to="/createquiz">
-        <div className="flex justify-center items-center pb-60">
+        <div className="flex justify-center items-center">
           <div className="inline-block rounded bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[2px] hover:text-white focus:outline-none focus:ring active:text-opacity-75 cursor-pointer">
             <span className="block rounded-sm bg-white px-60 py-3 text-2xl font-bold hover:bg-transparent">
               Create New Quiz
